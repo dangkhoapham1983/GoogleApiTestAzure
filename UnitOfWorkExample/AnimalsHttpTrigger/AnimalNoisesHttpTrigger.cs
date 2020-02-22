101a3ea8b6648292bfbb42eba9f382631878b436
Khoa <dangkhoapham1983@gmail.com>
2020-02-22 20:50:57 +0700

    Add Unit Test
    Add Unit Test

---
+++ UnitOfWorkExample/AnimalsHttpTrigger/AnimalNoisesHttpTrigger.cs
@@ -0,0 +1,25 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AzureFunctions.Autofac;
+using UnitOfWorkExample.Interfaces;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using UnitOfWorkExample.Configs;
+
+namespace UnitOfWorkExample.AnimalsHttpTrigger
+{
+    [DependencyInjectionConfig(typeof(AutofacConfig))]
+    public static class AnimalNoisesHttpTrigger
+    {
+        [FunctionName("AnimalNoisesHttpTrigger")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequestMessage req, TraceWriter log, [Inject]IAnimal animal)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+            return req.CreateResponse(HttpStatusCode.OK, animal.MakeNoise());
+        }
+    }
+}
---
+++ UnitOfWorkExample/Models/Cat.cs
@@ -0,0 +1,17 @@
+﻿using UnitOfWorkExample.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitOfWorkExample.Models
+{
+    public class Cat : IAnimal
+    {
+        public string MakeNoise()
+        {
+            return "Meow!";
+        }
+    }
+}
