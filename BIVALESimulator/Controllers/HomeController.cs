5bfce16b65c47f73848f670edb3f5fa530b5e83d
Khoa <dangkhoapham1983@gmail.com>
2020-02-24 11:58:01 +0700

    Add Simulator
    Add Simulator

---
+++ BIVALESimulator/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ReturnURL(string Email, string FirstName, string LastName, string GoogleID, string ProfileURL)
+        {
+			//Do your code for Signin or Signup
+			var data = new UserProfile();
+			data.Email = Email;
+			data.FirstName = FirstName;
+			data.LastName = LastName;
+			data.GoogleID = GoogleID;
+			data.ProfileURL = ProfileURL;
+
+			return Json(JsonConvert.SerializeObject(data), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ BIVALESimulator/Models/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+	public class UserProfile
+	{
+		public string Email { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string GoogleID { get; set; }
+		public string ProfileURL { get; set; }
+
+	}
+}
